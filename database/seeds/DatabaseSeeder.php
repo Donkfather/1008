057974456ecd4828f9cb740f54ebbd53c965f319
Tudor Popescu <tudor@adonissoft.com>
2018-08-10 01:31:23 +0300

    wip

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,34 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0');
-        Event::truncate();
-        \App\EventToken::truncate();
-        DB::statement('SET FOREIGN_KEY_CHECKS=1');
-        $events = [
-            [
-                'name' => 'Eveniment Global',
-                'start_date' => Carbon::now()->hour('13'),
-                'end_date' => Carbon::now()->addDay(1)->hour('4')
-            ],
-            [
-                'name' => 'Eveniment 1',
-                'start_date' => Carbon::now()->hour('13'),
-                'end_date' => Carbon::now()->hour('18')
-            ],
-            [
-                'name' => 'Eveniment 2',
-                'start_date' => Carbon::now()->hour('18'),
-                'end_date' => Carbon::now()->hour('19')
-            ],
-            [
-                'name' => 'Eveniment 3',
-                'start_date' => Carbon::now()->hour('19'),
-                'end_date' => Carbon::now()->hour('20')
-            ],
-        ];
-        collect($events)->each(function($event){
-            Event::create($event);
-        });
+
     }
 }
